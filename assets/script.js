bac762c1b27c80ae2b6c18b3e7137d51cda558ca
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-01-01 17:35:27 -0600

    Added buttons for answers

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//varibles for Event listeners
+var startbutton = document.querySelector("#startbutton")
+var answer1 = 
+//Varibles for the game
+//Function to start the game
+//Check to see if the answer is correct
+//Functon to end the game and record the score
