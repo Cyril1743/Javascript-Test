a645e3c296eea0f119e463bea43f839b514f3ed2
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-01-01 19:49:27 -0600

    Added funtion for varifing answers and added event listener for buttons

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
 var questionDiv = document.querySelector("#question")
 var time = document.querySelector("#timerspan")
+var answerResult = document.querySelector("#answerResult")
 var count = 60
 //Varibles for the game
 var chosenAnswer = "";
@@ -20,18 +21,82 @@ var answers = {
     5: ["A function", "A particular way to do something", "Nothing", "A systematic approach"],
     6: ["Dirty Old Man", "Date of Manufacture", "Document Object Model", "Distributed Order Management"]
 };
-var correctAnswer = ["To add functionality", "All the above", "Type and Value", "Return the remainder", "All the above", "A function", "Document Object Model"]
+var correctAnswer = ["To add functionality", "All the above", "Type and value", "Returns the remainder", "All the above", "A function", "Document Object Model"]
+//Event Listeners for buttons
+answer1.addEventListener("click", function(){
+    chosenAnswer = answer1.textContent
+    checkAnswer()
+})
+answer2.addEventListener("click", function(){
+    chosenAnswer = answer2.textContent
+    checkAnswer()
+})
+answer3.addEventListener("click", function (){
+    chosenAnswer = answer3.textContent
+    checkAnswer()
+})
+answer4.addEventListener("click", function (){
+    chosenAnswer = answer4.textContent
+    checkAnswer()
+})
+
 //Function to start the game
 startbutton.addEventListener("click", startGame)
 function startGame() {
+    questionNumber = 0
+    count = 60
     var timer = setInterval(function () {
         count--
         time.textContent = count
-        if (count === 0) {
+        if (count <= 0) {
             clearInterval(timer)
             endGame()
         }
     }, 1000)
+    startbutton.setAttribute("style", "display: none")
+    answer1.setAttribute("style", "display: block")
+    answer2.setAttribute("style", "display: block")
+    answer3.setAttribute("style", "display: block")
+    answer4.setAttribute("style", "display: block")
+    populateQuestion()
+}
+function populateQuestion() {
+    questionDiv.textContent = questions[questionNumber]
+    answer1.textContent = answers[questionNumber][0]
+    answer2.textContent = answers[questionNumber][1]
+    answer3.textContent = answers[questionNumber][2]
+    answer4.textContent = answers[questionNumber][3]
 }
 //Check to see if the answer is correct
+function checkAnswer() {
+    var answerTimerTime = 2
+    var answerTimer = setInterval(function(){
+        answerTimerTime --
+        if (answerTimerTime === 0){
+            clearInterval(answerTimer)
+            answerResult.textContent = ""
+        }
+        }, 1000)
+    if (chosenAnswer == correctAnswer[questionNumber]){
+        questionNumber++
+        count = count + 20
+        answerResult.textContent = "Correct!"
+        if (questionNumber === 7){
+            endGame()
+        } else {
+            populateQuestion()
+        }
+    } else {
+        questionNumber++
+        count = count - 10
+        answerResult.textContent = "Wrong!"
+        if (questionNumber === 7){
+            endGame()
+        } else {
+            populateQuestion()
+        }
+    }
+    }
+
+
 //Functon to end the game and record the score
